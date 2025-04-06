a34b483fe78df20fb142d7b684fe40a5dcf1ca40
sato1214syun <sato1214syun@gmail.com>
2025-04-06 11:01:16 +0900

    更新

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -477,6 +477,7 @@ fn merge_condition_data(csv_df: &DataFrame, condition_df: &DataFrame) -> LazyFra
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![write_excel])
         .run(tauri::generate_context!())
